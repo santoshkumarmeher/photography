cc7f37f7abf447121b526fbc2f3dd339ab008a0f
santoshkumarmeher <santoshkumarmeher13@gmail.com>
2025-09-11 20:45:41 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Load Navbar
+    fetch("navbar.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        });
+
+    // Load Footer
+    fetch("footer.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+});
